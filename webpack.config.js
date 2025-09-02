4a07d090faccc28d7a14a344163662a817357d60
lucstrike <lucaramosduarte@gmail.com>
2025-09-02 20:34:13 -0300

    fix handler

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   target: 'node',
-  entry: './src/functions/chloePreQualInbound.js',
+  entry: './index.js',
   output: {
     path: path.resolve(__dirname, '.webpack'),
     filename: 'handler.js',
