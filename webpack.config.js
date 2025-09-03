fd79aa5d05749a8d2be2d5eb71bb8dceb50e1deb
lucstrike <lucaramosduarte@gmail.com>
2025-09-02 22:57:04 -0300

    fix handler

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,9 @@ module.exports = {
   target: 'node',
   mode: 'production',
   entry: {
-    sendMessage: path.resolve(__dirname, 'src/functions/sendMessage.js'),
-    updatePrompt: path.resolve(__dirname, 'src/functions/updatePrompt.js'),
-  },
+  sendMessage: path.resolve(__dirname, 'src/functions/sendMessage.js'),
+  updatePrompt: path.resolve(__dirname, 'src/functions/updatePrompt.js'),
+},
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',       // nome do bundle será sendMessage.js e updatePrompt.js
