7dcc24276c64f33cebb7c7a5ad07053257238d26
lucstrike <lucaramosduarte@gmail.com>
2025-09-02 23:04:22 -0300

    fix handler

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
 const path = require('path');
 
-module.exports = {
-  target: 'node',
-  mode: 'production',
-  entry: {
-    sendMessage: path.resolve(__dirname, 'src/functions/sendMessage.js'),
-    updatePrompt: path.resolve(__dirname, 'src/functions/updatePrompt.js'),
+module.exports = [
+  {
+    entry: './src/functions/sendMessage.js',
+    target: 'node',
+    mode: 'production',
+    output: {
+      path: path.resolve(__dirname, 'dist/sendMessage'),
+      filename: 'index.js',
+      libraryTarget: 'commonjs2',
+    },
+    externals: [], // deixe axios, openai, etc
   },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    libraryTarget: 'commonjs2',
+  {
+    entry: './src/functions/updatePrompt.js',
+    target: 'node',
+    mode: 'production',
+    output: {
+      path: path.resolve(__dirname, 'dist/updatePrompt'),
+      filename: 'index.js',
+      libraryTarget: 'commonjs2',
+    },
+    externals: [],
   },
-  externals: [], // mantenha axios e openai fora se quiser bundle
-};
+];
