218e1525ea782512bbec23abbdf09e51bd189f7a
lucstrike <lucaramosduarte@gmail.com>
2025-09-02 21:03:58 -0300

    fix handler

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,12 @@
-// webpack.config.js
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js', // seu arquivo de entrada
-  target: 'node',          // importante para Lambda
+  entry: './src/functions/handler.js', // caminho correto do seu handler
+  target: 'node',
   mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',  // arquivo final que será enviado
+    filename: 'index.js',
     libraryTarget: 'commonjs2',
   },
-  externals: [], // aqui você pode deixar vazio para incluir axios e outras libs
 };
