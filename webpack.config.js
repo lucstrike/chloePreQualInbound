6ba3b5133f94aa0bfa133040ce4066b81764b387
lucstrike <lucaramosduarte@gmail.com>
2025-09-04 19:02:38 -0300

    teste completo

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
     entry: {
-    sendMessage: path.resolve(__dirname, 'src/functions/sendMessage.js'),
-    updatePrompt: path.resolve(__dirname, 'src/functions/updatePrompt.js'),
+    sendMessage: path.resolve(__dirname, './src/functions/sendMessage.js'),
+    updatePrompt: path.resolve(__dirname, './src/functions/updatePrompt.js'),
   },
   target: 'node',
   mode: 'production',
