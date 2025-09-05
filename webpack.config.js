aa5c2dcc8b09dd9ac87c521864f3d52c4ec20bcb
lucstrike <lucaramosduarte@gmail.com>
2025-09-04 21:41:58 -0300

    teste completo

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,14 @@ const path = require('path');
 
 module.exports = {
   entry: {
-  sendMessage: path.resolve(__dirname, './src/functions-lambda/sendMessage.js'),
-  updatePrompt: path.resolve(__dirname, './src/functions-lambda/updatePrompt.js'),
-},
+    sendMessage: path.resolve(__dirname, 'src/functions/sendMessage.js'),
+    updatePrompt: path.resolve(__dirname, 'src/functions/updatePrompt.js'),
+  },
   target: 'node',
   mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',       // Gera sendMessage.js e updatePrompt.js
-    libraryTarget: 'commonjs2',  // Para Node.js
-  },
-  externals: [], // deixe axios, openai, etc., se precisar bundlear
-  resolve: {
-    extensions: ['.js', '.json'], // permite importar sem escrever extensão
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader', // se quiser transpilar ES6
-      },
-    ],
+    filename: '[name].js',
+    libraryTarget: 'commonjs2',
   },
 };
