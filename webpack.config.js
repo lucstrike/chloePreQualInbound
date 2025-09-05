1d2a22e3ff006710ff930c5a180539560470fcc5
lucstrike <lucaramosduarte@gmail.com>
2025-09-04 21:48:52 -0300

    teste completo

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,13 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    sendMessage: path.resolve(__dirname, 'src/functions-lambda/sendMessage.js'),
-    updatePrompt: path.resolve(__dirname, 'src/functions-lambda/updatePrompt.js'),
-  },
-  target: 'node',          // Lambda roda Node.js
-  mode: 'production',      // produção = minificado
+  entry: './src/functions-lambda/sendmessage.js', // seu handler
+  target: 'node',
+  mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js', // cria sendMessage.js e updatePrompt.js
-    libraryTarget: 'commonjs2', // compatível com Lambda
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
   },
-  resolve: {
-    extensions: ['.js', '.json'],
-  },
-  externals: [],           // se quiser, pode listar libs que não precisam ser bundladas (ex: aws-sdk)
+  externals: [], // IMPORTANTE: não excluir axios do bundle
 };
